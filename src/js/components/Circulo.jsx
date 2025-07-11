e6c38039ef44f4c6d3f62b2a335ef15784c2d080
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-07-11 12:57:44 +0000

    semaforo

---
+++ src/js/components/Circulo.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+
+export const Circulo = () => {
+
+    const [color, setColor] = useState('')
+
+    return (
+        <>
+
+            <div className="soporteSemaforo mx-auto"></div>
+            <div className="semaforo container rounded-4 text-center">
+                <div>
+                    <div className="p-3 text-center d-flex flex-column gap-4">
+                        <button onClick={()=> setColor('red')} className={`btn rounded-circle mx-auto btn-secondary ${color === 'red' ? 'red' : '' }`}
+                            style={{
+                                width: '13rem',
+                                height: '13rem'
+                            }}>
+                        </button>
+                        
+                        <button onClick={()=> setColor('orange')} className={`btn rounded-circle mx-auto btn-secondary ${color === 'orange' ? 'orange' : '' }`}
+                            style={{
+                                width: '13rem',
+                                height: '13rem',
+                            }}>
+                        </button>
+                        
+                        <button onClick={()=> setColor('green')} className={`btn rounded-circle mx-auto btn-secondary ${color === 'green' ? 'green' : '' }`}
+                            style={{
+                                width: '13rem',
+                                height: '13rem',
+                            }}></button>
+                    </div>
+                    
+                </div>
+                <button className="btn btn-danger text-white mx-auto btn-lg mt-4">Alternar colores</button>
+            </div>
+                            
+        </>
+
+    )
+};
